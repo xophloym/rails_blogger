63645b38ce733001f21329e943656bf1d547b306
julius <xophloym@gmail.com>
2017-09-26 10:57:50 +0900

    first blogger commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,15 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+
+  end
+
+
+end
